9bd9a2359a34c93437685ff237e8f7f8b715a02e
Caleb Van Lue <cpvanlue@bsu.edu>
2021-09-02 23:05:47 -0400

    Refactor RR

--- src/RR.java
+++ src/RR.java
@@ -11,7 +11,7 @@ public class RR implements Algorithm
     private final Queue<Process> processesToSchedule;
     private final HashMap<String, Integer> originalBursts;
     private int totalNumProcesses;
-    private final int timeQuantum = 10;
+    private final int timeQuantum = 5;
 
     public RR(List<Process> allProcessList) {
         readyQueue = new LinkedList<>();
@@ -23,17 +23,16 @@ public class RR implements Algorithm
 
     public void schedule() {
         System.out.println("Round Robin scheduling");
+        totalNumProcesses = processesToSchedule.size();
         int totalWaitingTime = 0;
-        Process currentProcess;
         Process p = processesToSchedule.remove();
         if (CPU.getCurrentTime() < p.getArrivalTime()) {
             CPU.advanceTimeTo(p.getArrivalTime());
         }
         readyQueue.add(p);
         while (!readyQueue.isEmpty()) {
-            currentProcess = pickNextProcess();
+            Process currentProcess = pickNextProcess();
             if (!originalBursts.containsKey(currentProcess.getName())) {
-                totalNumProcesses++;
                 originalBursts.put(currentProcess.getName(), currentProcess.getCPUBurstTime());
             }
             CPU.run(currentProcess, Math.min(currentProcess.getCPUBurstTime(), timeQuantum));
@@ -43,8 +42,7 @@ public class RR implements Algorithm
                     readyQueue.add(q);
                 }
             }
-            int waitingTime = CPU.getCurrentTime() - currentProcess.getArrivalTime() - originalBursts.get(currentProcess.getName());
-            totalWaitingTime += waitingTime;
+            totalWaitingTime += CPU.getCurrentTime() - currentProcess.getArrivalTime() - originalBursts.get(currentProcess.getName());;
             processesToSchedule.removeAll(readyQueue);
             if (currentProcess.getCPUBurstTime() <= 0) {
                 System.out.println(currentProcess.getName() + " has finished at time " + CPU.getCurrentTime() + " with a waiting time of " +
@@ -61,7 +59,7 @@ public class RR implements Algorithm
                 }
             }
         }
-        System.out.printf("The average waiting time is %.2f", (double) totalWaitingTime / totalNumProcesses);
+        System.out.printf("The average waiting time is %.2f\n", (double) totalWaitingTime / totalNumProcesses);
     }
 
     public Process pickNextProcess() {
